7c9b69701afb8b67d4451e8c3691b3a76e8cb69a
Matt Rostad <matt@rostad.ca>
2023-03-23 17:06:04 -0700

    with client.js file

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "192.168.1.88",
+    port: 50541, // PORT number here,
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  conn.on("connect", () => {
+    console.log("Connection Established");
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = {
+  connect,
+};
