1adc7e06138a69b5622942d9b3316dcccea5b1cb
Matt Rostad <matt@rostad.ca>
2023-03-24 12:38:15 -0700

    set up stdin

--- client.js
+++ client.js
@@ -26,6 +26,24 @@ const connect = function () {
   return conn;
 };
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key) {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
 module.exports = {
   connect,
+  setupInput
 };
--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 // establishes a connection with the game server
-const {connect} = require('./client');
+const {connect, setupInput} = require('./client');
 
 console.log("Connecting ...");
 connect();
+setupInput()
